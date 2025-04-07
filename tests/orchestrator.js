2089c510ef240951aa02e7896150f8234aa6ad80
Giorge Lucas <giorgelucasx@gmail.com>
2025-04-07 20:50:44 -0300

    make `npm test` faster

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,6 +6,7 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
@@ -17,4 +18,4 @@ async function waitForAllServices() {
 
 export default {
   waitForAllServices,
-};
+}; //#121
